6c71fc3bf6988c1b622138bb952a63734197baf6
Jeffrey Lin <>
2021-05-15 20:42:04 +0000

    condensed code

--- main.py
+++ main.py
@@ -1,65 +1,52 @@
 import os
 import discord
 import time
+from discord.ext import commands
 
-client = discord.Client()  
+client = commands.Bot(command_prefix='!') 
+
+@client.command(name='study')
+async def study(context):
+  global isStudying
+  command = context.message.content.split()
+  channel = context.message.channel
+  try:
+    isStudying = True
+    studyTime = int(command[1]) * 1
+    breakTime = int(command[2]) * 1
+    cycles = int(command[3]) * 2
+    await channel.send('Study time!')
+    start = time.time()
+    timer = studyTime
+    state = 1    
+
+    while cycles > 0:
+      if (time.time() - start) >= timer:
+        cycles = cycles - 1
+        if state == 1:
+          state = 0
+          timer = breakTime
+          if cycles != 0:
+            await channel.send('Break time!')
+        else:
+          state = 1
+          timer = studyTime
+          if cycles != 0:
+            await channel.send('Study time!')
+        start = time.time()
+    await channel.send('Good study session')
+  except:
+    await channel.send('!study [Duration of studying] [Duration of break] [Cycles]')
+    await channel.send('Recommended: !study 30 5 4')
+  
+@client.command(name='hi')
+async def hi(context):
+  await context.message.channel.send("Hi")
+
+
 
 @client.event
 async def on_ready():
-  print('Hello, Jeffury is ready to help you study'.format(client))
-
-@client.event
-async def on_message(message):
-
-  global studyTime
-  global breakTime
-  global isStudying
-  global msg
-
-  msg = message
-
-  if message.author == client.user:
-    return
-
-  if message.content == '!quit':
-    isStudying = False
-    await message.channel.send('Good study session!')
-
-  if message.content.startswith('!study'):
-    isStudying = True
-    command = message.content.split()
-
-    try:
-      studyTime = int(command[1] * 1)
-      breakTime = int(command[2] * 1)
-      await message.channel.send('Study time!')
-      await studySession()
-    except:
-      await message.channel.send('Sorry I don\'t understand')
-
-async def studySession():
-  global isStudying
-  global onBreak
-  global start
-  global studyTime
-  global msg
-
-  start = time.time()
-  timer = studyTime
-  state = 1    
-
-  while isStudying:
-    if not isStudying:
-      break
-    if (time.time() - start) >= timer:
-      if state == 1:
-        state = 0
-        timer = breakTime
-        await msg.channel.send('Break time!')
-      else:
-        state = 1
-        timer = studyTime
-        await msg.channel.send('Study time!')
-      start = time.time()
+  print('Bot is online')
 
 client.run(os.environ['token'])
