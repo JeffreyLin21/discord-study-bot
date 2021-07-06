31e6318039e4ab085c4355320b9f7772eb8c4e60
Jeffrey Lin <42254833+JeffreyLin21@users.noreply.github.com>
2021-07-06 01:51:45 +0000

    msg now get send directly to user

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import discord
 import time
 import asyncio
 from discord.ext import commands
-from webserver import refresh
 
 client = commands.Bot(command_prefix='!') 
 client.remove_command('help')
@@ -18,7 +17,12 @@ async def help(context):
   help_embed.add_field(name = '!quit', value = 'end current study session', inline = False) 
   help_embed.add_field(name = '!status', value = 'check timer of current study session', inline = False)
 
-  await context.message.channel.send(embed = help_embed) 
+  await context.message.author.send(embed = help_embed) 
+
+@client.command(name='start')
+async def start(context):
+  msg_embed = discord.Embed(title = 'Hello I am Study Bot', description = "created by Jeffrey Lin", color = 0x831b6d)
+  await context.message.author.send(embed = msg_embed)
 
 @client.command(name='status')
 async def status(context):
@@ -27,7 +31,7 @@ async def status(context):
     timer_embed = discord.Embed(title = 'Timer', description = str(int(timer-(time.time()-start))//60) + ':' + str(int(round(((timer-(time.time()-start))%60), 0))) + ' minutes remaining', color = 0x831b6d)
   except:
     timer_embed = discord.Embed(title = 'Looks like a study session hasn\'t started yet.')
-  await context.message.channel.send(embed = timer_embed)
+  await context.message.author.send(embed = timer_embed)
 
 @client.command(name='study')
 async def study(context, *args):
@@ -38,7 +42,7 @@ async def study(context, *args):
   isStudying = True
   state = 1    
   start = time.time() 
-  channel = context.message.channel
+  author = context.message.author
 
   try:
 
@@ -64,7 +68,7 @@ async def study(context, *args):
 
     quit_embed = discord.Embed(title = 'Good session!', color=0x831b6d)
 
-    await channel.send(embed = study_embed)
+    await author.send(embed = study_embed)
     start = time.time()
     timer = studyTime
     
@@ -76,22 +80,22 @@ async def study(context, *args):
           state = 0
           timer = breakTime
           if isStudying and cycles > 0:
-            await channel.send(embed = break_embed)
+            await author.send(embed = break_embed)
         else:
           state = 1
           timer = studyTime
           if isStudying and cycles > 0:
-            await channel.send(embed = study_embed)
+            await author.send(embed = study_embed)
         start = time.time()
-    await channel.send(embed = quit_embed)
+    await author.send(embed = quit_embed)
   except Exception as e: 
     print(e)
     if start == ' ':
-      await channel.send(embed = quit_embed)
+      await author.send(embed = quit_embed)
     else:
       end_embed = discord.Embed(description = '!study [Duration of studying] [Duration of break] [Cycles]', color=0x831b6d)
       end_embed.set_footer(text = 'Leave all field blank for recommened settings !study 30 5 4 or leave cycles blank for an infinite session')
-      await channel.send(embed = end_embed)
+      await author.send(embed = end_embed)
 
 @client.command(name='quit')
 async def quit(context):
@@ -109,13 +113,12 @@ async def remind(context, *args):
       message += (i + ' ')
     while (time.time() - current <= int(args[0])*60):
       await asyncio.sleep(1)
-    print(message)
     msg_embed = discord.Embed(description = message, color=0x831b6d)
-    await context.message.channel.send(embed = msg_embed)
+    await context.message.author.send(embed = msg_embed)
   except Exception as e: 
     print(e)
     end_embed = discord.Embed(description = '!remind [Number of minutes to remind in] [Message to send]', color=0x831b6d)
-    await context.message.channel.send(embed = end_embed)
+    await context.message.author.send(embed = end_embed)
   
 
 
@@ -124,5 +127,4 @@ async def remind(context, *args):
 async def on_ready():
   print('Bot is online')
 
-#refresh()
 client.run(os.environ['token'])
